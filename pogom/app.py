08c991777be9588971c7c8771e9ee03b64e10e6e
RatedRion <ratedrion@gmail.com>
2016-07-23 18:51:40 -0400

    removed ScannedLocation

--- pogom/app.py
+++ pogom/app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from s2sphere import *
 
 from . import config
-from .models import Pokemon, Gym, Pokestop, ScannedLocation
+from .models import Pokemon, Gym, Pokestop
 
 
 class Pogom(Flask):
@@ -38,9 +38,6 @@ class Pogom(Flask):
 
         #if request.args.get('gyms', 'true') == 'true':
         #    d['gyms'] = Gym.get_all()
-
-        #if request.args.get('scanned', 'true') == 'true':
-        #    d['scanned'] = ScannedLocation.get_recent()
 
         return jsonify(d)
 
